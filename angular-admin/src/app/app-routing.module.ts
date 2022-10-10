caaad5e92cba1d99c74912e22b4ce8730a9795ab
Eric <shonga@msn.com>
2022-10-10 17:05:24 +0200

    feat: redirection

--- angular-admin/src/app/app-routing.module.ts
+++ angular-admin/src/app/app-routing.module.ts
@@ -6,13 +6,16 @@ import { RegisterComponent } from './public/register/register.component';
 import { DashboardComponent } from './secure/dashboard/dashboard.component';
 import { ProfileComponent } from './secure/profile/profile.component';
 import { SecureComponent } from './secure/secure.component';
+import { UsersComponent } from './secure/users/users.component';
 
 const routes: Routes = [
   {
     path: '', 
   component: SecureComponent,
   children: [
-    {path: '', component: DashboardComponent},
+    {path: '', redirectTo:'/dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'users', component: UsersComponent},
     {path: 'profile', component: ProfileComponent},
   ]
 },
