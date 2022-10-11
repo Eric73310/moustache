274a56eda2a3c24dcb6a61d1eeb1f201b8c37fec
Eric <shonga@msn.com>
2022-10-11 11:30:15 +0200

    feat: delete users + pagination users

--- angular-admin/src/app/secure/users/users.component.ts
+++ angular-admin/src/app/secure/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[] = [];
+  page = 1;
+  lastPage: number;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.load();
+  }
+
+  load(): void {
+    this.userService.all(this.page).subscribe(
+      (res: any) => {
+        this.users = res.data;
+        this.lastPage = res.meta.last_page;
+      }
+    );
+  }
+
+  next(): void {
+    if(this.page === this.lastPage) {
+      return;
+    }
+
+    this.page++;
+    this.load();
+  }
+
+  prev(): void {
+    if(this.page === 1) {
+      return;
+    }
+    
+    this.page--;
+    this.load();
+  }
+
+  delete(id:number): void {
+    if(confirm('Are you sure ?')) {
+      this.userService.delete(id).subscribe(
+        () => {
+            this.users = this.users.filter(u => u.id !== id);
+        }
+      );
+    }
   }
 
 }
